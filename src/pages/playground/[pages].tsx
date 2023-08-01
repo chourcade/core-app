346d2260c31af83e7e47a978d52208e741d25285
Cristian Hourcade <chourcade@Cristians-MacBook-Pro.local>
2023-08-01 15:47:01 -0300

    Dinamic paths

---
+++ src/pages/playground/[pages].tsx
@@ -0,0 +1,20 @@
+// @ts-nocheck
+
+import dynamic from 'next/dynamic';
+
+const RemotePage = dynamic(() => import('playground/Playground'));
+
+export default function PlaygroundPage(props) {
+  return <RemotePage {...props} />
+}
+
+export const getServerSideProps = async (ctx) => {
+  const remotePage = await import('playground/Playground');
+  if (remotePage.getServerSideProps) {
+    return remotePage.getServerSideProps(ctx)
+  }
+
+  return {
+    props: { path: ctx.resolvedUrl.replaceAll('/playground','') },
+  }
+}
